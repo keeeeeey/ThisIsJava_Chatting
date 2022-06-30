33e0087677e408417449661479ec3025a6465dc8
keeeeeey <sseioul@naver.com>
2022-06-30 11:35:14 +0900

    [FEAT] is, os 추가

--- src/chatting/ServerExample.java
+++ src/chatting/ServerExample.java
@@ -1,6 +1,8 @@
 package chatting;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -17,6 +19,26 @@ public class ServerExample {
 				Socket socket = serverSocket.accept();
 				InetSocketAddress isa = (InetSocketAddress) socket.getRemoteSocketAddress();
 				System.out.println("[연결 수락함] " + isa.getHostName());
+				
+				byte[] bytes = null;
+				String message = null;
+				
+				InputStream is = socket.getInputStream();
+				bytes = new byte[100];
+				int readByteCount = is.read(bytes);
+				message = new String(bytes, 0, readByteCount, "UTF-8");
+				System.out.println("[데이터 받기 성공]: " + message);
+				
+				OutputStream os = socket.getOutputStream();
+				message = "Hello Client";
+				bytes = message.getBytes("UTF-8");
+				os.write(bytes);
+				os.flush();
+				System.out.println("[데이터 보내기 성공]");
+				
+				is.close();
+				os.close();
+				socket.close();
 			}
 		} catch (Exception e) {
 			
